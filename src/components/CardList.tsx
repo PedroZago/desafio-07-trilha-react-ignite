4ea0affcb66c0e1047bbe5594cc53913a6c66ee7
Pedro Zago <pedro.camargozago@hotmail.com.br>
2022-06-07 08:43:34 -0300

    Basic requirements

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -16,17 +16,42 @@ interface CardsProps {
 }
 
 export function CardList({ cards }: CardsProps): JSX.Element {
-  // TODO MODAL USEDISCLOSURE
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-  // TODO SELECTED IMAGE URL STATE
+  const [selectedImageUrl, setSelectedImageUrl] = useState('');
 
-  // TODO FUNCTION HANDLE VIEW IMAGE
+  const handleViewImage = (url: string) => {
+    onOpen();
+    setSelectedImageUrl(url);
+  }
 
   return (
     <>
       {/* TODO CARD GRID */}
+      <SimpleGrid
+        columns={[1, 2, 3]}
+        spacing="40px"
+      >
+        {
+          cards?.map(card => {
+            return (
+              <Card
+                key={card.id}
+                data={card}
+                viewImage={handleViewImage}
+              />
+            )
+          })
+        }
+
+      </SimpleGrid>
 
       {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage
+        isOpen={isOpen}
+        onClose={onClose}
+        imgUrl={selectedImageUrl}
+      />
     </>
   );
 }
